2809993660f17666643ae319e9b149276f3eaa63
Ciaran <ciarantrotman@gmail.com>
2018-03-05 17:06:50 +0000

    Transition First Pass

---
+++ Assets/10 Patterns/Pattern Specific Scripts/TG1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TG1 : MonoBehaviour
+{
+    public GameObject TransitionContent;
+    private GameObject UserCenter;
+    private GameObject EgocentricContentOrigin;
+    public float TransitionThreshold;
+    private float ContentDistance;
+
+    public void Start()
+    {
+        EgocentricContentOrigin = GameObject.Find("Egocentric Content Origin");
+        UserCenter = GameObject.Find("User Center");
+    }
+
+    public void Update()
+    {
+        Debug.Log(ContentDistance + " | " + TransitionThreshold);
+        ContentDistance = Vector3.Distance(TransitionContent.transform.position, UserCenter.transform.position);
+        if (ContentDistance < TransitionThreshold)
+        {
+            TransitionContent.transform.parent = EgocentricContentOrigin.transform;
+        }
+        else
+        {
+            TransitionContent.transform.parent = null;
+        }
+    }
+}
---
+++ Assets/10 Patterns/Pattern Specific Scripts/TX1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TX1 : MonoBehaviour {
+
+    private Rigidbody rigidbody;
+    public GameObject TransitionContent;
+    private GameObject UserCenter;
+    private GameObject EgocentricContentOrigin;
+    public float TransitionThreshold;
+    private float ContentDistance;
+
+    public void Start()
+    {
+        rigidbody = TransitionContent.GetComponent<Rigidbody>();
+        EgocentricContentOrigin = GameObject.Find("Egocentric Content Origin");
+        UserCenter = GameObject.Find("User Center");
+    }
+
+    public void Update()
+    {
+        Debug.Log(ContentDistance + " | " + TransitionThreshold);
+        ContentDistance = Vector3.Distance(TransitionContent.transform.position, UserCenter.transform.position);
+        if (ContentDistance < TransitionThreshold)
+        {
+            rigidbody.drag = 3;
+            TransitionContent.transform.parent = EgocentricContentOrigin.transform;
+        }
+        else
+        {
+            rigidbody.drag = 100;
+            TransitionContent.transform.parent = null;
+        }
+    }
+}
