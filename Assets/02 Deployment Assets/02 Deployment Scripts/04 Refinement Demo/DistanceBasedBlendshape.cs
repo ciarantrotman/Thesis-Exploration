d7db255df95a81b195662230dfd9acd2258706a1
Ciaran <ciarantrotman@gmail.com>
2018-04-12 14:34:21 +0100

    UI Previs | Email, Article, General & Contextual Shell

--- Assets/02 Deployment Assets/02 Deployment Scripts/04 Refinement Demo/DistanceBasedBlendshape.cs
+++ Assets/02 Deployment Assets/02 Deployment Scripts/04 Refinement Demo/DistanceBasedBlendshape.cs
@@ -5,30 +5,45 @@ using UnityEngine;
 public class DistanceBasedBlendshape : MonoBehaviour
 {
     private GameObject _blendshapeObject;
-    private GameObject _userHand;
-    private float _distance;
+    private GameObject _rightHand;
+    private GameObject _leftHand;
+    private float _rightDistance;
+    private float _leftDistance;
     private float _distancePercentage;
     private float _blendshapeValue;
     private int _blendshapeIndex = 0;
     SkinnedMeshRenderer skinnedMeshRenderer;
     [Header("Distance Based Blendshape Settings")]
     [Space(5)]
+    public bool _leftHandTrigger;
+    public bool _rightHandTrigger = true;
+    public GameObject _altTrigger;
     public float _distanceThreshold = .2f;
     void Start ()
     {
         skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
         _blendshapeObject = gameObject;
-        _userHand = GameObject.Find("Right Hand Proxy");
+        if (_leftHandTrigger == false && _rightHandTrigger == false)
+        {
+            _rightHand = _altTrigger;
+            _leftHand = _altTrigger;
+        }
+        else
+        {
+            _rightHand = GameObject.Find("Right Hand Proxy");
+            _leftHand = GameObject.Find("Left Hand Proxy");
+        }
     }
 	void Update ()
     {
-        _distance = Vector3.Distance(_blendshapeObject.transform.position, _userHand.transform.position);
-        if (_distance < _distanceThreshold)
+        _rightDistance = _rightHandTrigger ? Vector3.Distance(_blendshapeObject.transform.position, _rightHand.transform.position) : 100;
+        _leftDistance = _leftHandTrigger ? Vector3.Distance(_blendshapeObject.transform.position, _leftHand.transform.position) : 100;
+        if (_rightDistance < _distanceThreshold || _leftDistance < _distanceThreshold)
         {
-            _blendshapeValue = (_distance/_distanceThreshold)*100;
+            _blendshapeValue = (_rightDistance/_distanceThreshold)*100;
             skinnedMeshRenderer.SetBlendShapeWeight(_blendshapeIndex, _blendshapeValue);
         }
-        if (_distance >= _distanceThreshold)
+        if (_rightDistance >= _distanceThreshold && _leftDistance >= _distanceThreshold)
         {
             skinnedMeshRenderer.SetBlendShapeWeight(_blendshapeIndex, 100);
         }
