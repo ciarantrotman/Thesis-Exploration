4d233cb7a6c867cb3a7dd8dd1713671bac28833d
Ciaran <ciarantrotman@gmail.com>
2018-04-18 22:16:22 +0100

    Pre-Testing UI Pass

---
+++ Assets/02 Deployment Assets/02 Deployment Scripts/04 Refinement Demo/BrowserSlider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using Leap.Unity.Attributes;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using Leap.Unity.Interaction;
+using UnityEngine.Events;
+
+public class BrowserSlider : MonoBehaviour
+{
+    public UnityEvent _left;
+    public UnityEvent _center;
+    public UnityEvent _right;
+    public GameObject SliderObject;
+    private float SliderMultiplier = 100.0f;
+    private InteractionSlider HorizontalSlider;
+    private float _slidervalue;
+
+    void Update()
+    {
+        HorizontalSlider = SliderObject.GetComponent<InteractionSlider>();
+        _slidervalue = HorizontalSlider.HorizontalSliderPercent;
+        _slidervalue = _slidervalue * SliderMultiplier;
+        if (_slidervalue < 33)
+        {
+            _left.Invoke();
+        }
+        if (_slidervalue > 33 && _slidervalue < 65)
+        {
+            _center.Invoke();
+        }
+        if (_slidervalue > 66)
+        {
+            _right.Invoke();
+        }
+    }
+}
---
+++ Assets/02 Deployment Assets/02 Deployment Scripts/04 Refinement Demo/TimeText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeText : MonoBehaviour {
+
+    private int _tm = 0;
+    private int _m = 0;
+    private int _ts = 0;
+    private int _s = 0;
+    void Update ()
+    {
+        TextMeshPro textmeshPro = GetComponent<TextMeshPro>();
+        if (Time.time % 10 == 0)
+        {
+            _s++;
+        }
+        if (_s % 10 == 0)
+        {
+            _s = 0;
+            _ts++;
+        }
+        if (_ts % 6 == 0)
+        {
+            _ts = 0;
+            _m++;
+        }
+        if (Time.time % 60 == 0)
+        {
+            _m++;
+        }
+        else if (Time.time > 9)
+        {
+            
+        }
+        textmeshPro.SetText("0{0}:{1}{2}", _tm, _ts, _s);
+    }
+}
