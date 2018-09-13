3832a3d8affdc7687fa434743dc442dca4679342
Ciaran <ciarantrotman@gmail.com>
2018-09-13 09:48:54 +0100

    Procedural Mesh Generation

---
+++ Assets/Traces Assets/Scripts/DynamicMesh.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicMesh : MonoBehaviour
+{
+	private MeshFilter _mf;
+	private Mesh _m;
+	[Header("Vertex Positions")]
+	[Tooltip("Ensure they are in the the correct order")]
+	public List<GameObject> MeshVertices;
+	
+	private void Start ()
+	{
+		_mf = GetComponent<MeshFilter>();
+		_m = _mf.mesh;
+	}
+
+	private void Update ()
+	{
+		_m.vertices = new Vector3[]
+		{
+			MeshVertices[0].transform.localPosition, 
+			MeshVertices[1].transform.localPosition, 
+			MeshVertices[2].transform.localPosition,
+			MeshVertices[3].transform.localPosition,
+		};
+		_m.triangles = new int[]
+		{
+			0,2,3,
+			3,1,0
+		};
+	}
+}
---
+++ Assets/Traces Assets/Scripts/LineGizmo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class LineGizmo : MonoBehaviour
+{
+	public Transform Target;
+	public Color LineColor;
+
+	private void Update () 
+	{
+		if (Target != null)
+			Debug.DrawLine(transform.position, Target.transform.position, LineColor);
+	}
+}
